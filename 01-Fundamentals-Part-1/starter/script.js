a73d308797b0575575fe66ff13495a37c00328b0
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-07 01:18:47 +0200

    values & variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,30 @@
+////////////////////////////////////
+// Linking a JavaScript File
+
+// if (js === 'amazing') alert('Javascript is FUN!');
+
 let js = 'amazing';
-
-if (js === 'amazing') alert('Javascript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log('Jakub');
+console.log(23);
+
+let firstName = 'Jakub';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let person = 'jakub';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
