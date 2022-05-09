66ebe3231e563a8a2dcead6095fb0deea678fe1e
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-09 16:44:17 +0200

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,8 @@
 ////////////////////////////////////
 // Linking a JavaScript File
-
-// if (js === 'amazing') alert('Javascript is FUN!');
-
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
+// if (js === 'amazing') alert('Javascript is FUN!');
 
 ////////////////////////////////////
 // Values and Variables
@@ -28,3 +26,58 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+// 1
+
+let country = 'Poland';
+let continent = 'Europe';
+let population = 3795000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+////////////////////////////////////
+// Data Types
+let javascriptIsFun = true; // boolean
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jakub');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994;
+console.log(typeof year);
+
+console.log(typeof null); // undefined its ERROR in JS
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(language);
+
+////////////////////////////////////
+// let, const and var
