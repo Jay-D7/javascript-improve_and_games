dcc07c4a6ad13a84ef6fe5857a7bdbbfc40d9fce
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-09 20:17:15 +0200

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -312,3 +312,20 @@ console.log(Boolean(undefined));
 console.log(Boolean('Jakub'));
 console.log(Boolean({}));
 console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+  console.log("Don't spent it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
+
+////////////////////////////////////
+// Equality Operators: == vs. ===
