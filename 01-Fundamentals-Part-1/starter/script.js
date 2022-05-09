894b7d997d24b50cd5641b057de086d72b9e56cf
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-09 20:04:53 +0200

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -277,3 +277,38 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is Higher than Mark's (${BMIMark})!`);
 }
+
+console.log('\n' + '\n' + '\n');
+
+////////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jakub'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+
+console.log('\n' + '\n' + '\n');
+////////////////////////////////////
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jakub'));
+console.log(Boolean({}));
+console.log(Boolean(''));
