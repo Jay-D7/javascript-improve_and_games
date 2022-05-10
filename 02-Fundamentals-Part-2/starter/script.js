3b918c81e416d62d3441e73cb51a2928a429f1dd
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-10 23:33:11 +0200

    Activating Strict Mode

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive 💪🏻');
+
+// CANT USE SOME WORDS FOR VARIABLES
+// const interface = 'Audio';
+// const private = 534;
+
+///////////////////////////////////////
+// Functions
