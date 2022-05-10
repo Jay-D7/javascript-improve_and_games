fb399c7c6e55c0cdd212bb7826eb213b34182e07
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-11 00:07:20 +0200

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -14,3 +14,38 @@ if (hasDriversLicense) console.log('I can drive 💪🏻');
 
 ///////////////////////////////////////
 // Functions
+function logger() {
+  console.log('My name is Jacob');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23'); // Built-in function
+// console.log('') // Built-in function
+
+// LECTURE: Functions
+function describeCountry(country, population, capitalCity) {
+  const sentence1 = `${country} has ${population} milion people and its capital city is ${capitalCity}`;
+  return sentence1;
+}
+
+const descPoland = describeCountry('Poland', 39, 'Warsaw');
+const descGermany = describeCountry('Germany', 83, 'Berlin');
+const descUkraine = describeCountry('Ukraine', 44, 'Kyiv');
+console.log(descPoland);
+console.log(descUkraine);
+console.log(descGermany);
