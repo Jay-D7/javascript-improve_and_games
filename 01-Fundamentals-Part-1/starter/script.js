40e19e39a274d9eaf19b83501bdee810af9a36e8
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-10 17:41:09 +0200

    Statements and Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -524,3 +524,17 @@ switch (language1) {
     console.log('Great language too');
     break;
 }
+
+////////////////////////////////////
+// Statements and Expressions
+
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jakub';
+console.log(`I'm ${2037 - 1994} years old ${me}`);
