450ba9f429e09792f4644172294b5dd8d26f4042
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-11 16:17:50 +0200

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -37,6 +37,7 @@ console.log(appleOrangeJuice);
 const num = Number('23'); // Built-in function
 // console.log('') // Built-in function
 
+//
 // LECTURE: Functions
 function describeCountry(country, population, capitalCity) {
   const sentence1 = `${country} has ${population} milion people and its capital city is ${capitalCity}`;
@@ -67,6 +68,7 @@ const age2 = calcAge2(1994);
 
 console.log(age1, age2);
 
+//
 // LECTURE: Function Declarations vs. Expressions
 function percentageOfWorld1(population) {
   return (population / 7900) * 100;
@@ -85,3 +87,33 @@ console.log(percPeru1);
 
 ///////////////////////////////////////
 // Arrow functions
+// Arrow functions don't have 'this' keyword
+
+const calcAge3 = (birthYeah) => 2022 - birthYeah;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2022 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1994, 'Jakub'));
+console.log(yearsUntilRetirement(1980, 'John'));
+
+//
+// LECTURE: Arrow Functions
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const percPortugal3 = percentageOfWorld3(100);
+const percChina3 = percentageOfWorld3(1438);
+const percUSA3 = percentageOfWorld3(332);
+console.log(percPortugal3, percChina3, percUSA3);
+
+///////////////////////////////////////
+// Functions Calling Other Functions
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
