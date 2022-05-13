9362a48a5ea5d0f3e7d823c7e8318fb63bfc7925
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-13 02:17:55 +0200

    Introduction to Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -348,3 +348,21 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, totals);
 console.log(totals);
+
+///////////////////////////////////////
+// Introduction to Objects
+const JohnArray = [
+  'John',
+  'Wick',
+  1945 - 2037,
+  'Assasin',
+  ['Peter', 'Tony', 'Martha'],
+];
+
+const John = [
+  'John',
+  'Wick',
+  1945 - 2037,
+  'Assasin',
+  ['Peter', 'Tony', 'Martha'],
+];
