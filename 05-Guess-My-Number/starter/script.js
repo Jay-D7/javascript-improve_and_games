16dc6d5067ba10e52f6eebf134a4d88b4881f80d
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-30 18:38:05 +0200

    #Selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,4 +3,10 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct number!';
 
-document.querySelector('.number');
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
