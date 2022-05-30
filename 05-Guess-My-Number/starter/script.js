74ac97026469c92ac37505f19b1ca5307a2a462f
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-30 19:54:43 +0200

    #Implementic the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,14 +11,42 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-const x = function () {};
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // always between 1 - 20!!
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number';
     // displayMessage('⛔️ No number');
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      // score = score - 1; the same under
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔 You lose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too low!';
+      // score = score - 1; the same under
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔 You lose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
