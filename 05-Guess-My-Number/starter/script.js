afb731734760f36bb8c0ae2d95f87307d9da9352
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-30 17:55:36 +0200

    PROJECT #1: Guess My Number!

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+
+document.querySelector('.number');
