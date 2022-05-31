4b8b5c1250a904333ae50abd78fb63f0018afad4
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-31 20:00:58 +0200

    Project#2 Modal window add variables and loop

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function logger() {
+  console.log('Start');
+}
+// logger();
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal'); //Important All - gives you access to nodeList
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
+
+// const openModal = function () {
+//   modal.classList.add('hidden');
+// };
