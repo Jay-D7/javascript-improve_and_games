d2ee04d7351ff3c2115ca10bcd12ebdbf7c10799
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-05-31 15:20:25 +0200

    #Implementing highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,7 +14,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // always between 1 - 20!!
 let score = 20;
-// let highscore = 0;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -32,6 +32,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
