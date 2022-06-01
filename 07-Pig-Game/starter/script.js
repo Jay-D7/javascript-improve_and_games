93d7f9bb07ce5a9e053bf4bb3d3cbfcb784a2779
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-06-01 18:30:22 +0200

    Rolling the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,24 +1,43 @@
 'use strict';
 
 // Selecting elements
-// const player0El = document.querySelector('.player--0');
-// const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-// const current0El = document.getElementById('current--0');
-// const current1El = document.getElementById('current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+// const player0El = document.querySelector('.player--0');
+// const player1El = document.querySelector('.player--1');
 
 //Starting conditions
-// const btnNew = document.querySelector('.btn--new');
-// const btnRoll = document.querySelector('.btn--roll');
-// const btnRHold = document.querySelector('.btn--hold');
-
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// console.log(score0El);
-// console.log(score1El);
-// console.log(diceEl);
-// score0El;
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice; // Same but faster & quality better than below
+    current0El.textContent = currentScore; // TODO CHANGE LATER
+    // currentScore = currentScore + dice;
+  } else {
+    //Switch to next player
+    currentScore += dice;
+    current1El.textContent = currentScore;
+  }
+});
