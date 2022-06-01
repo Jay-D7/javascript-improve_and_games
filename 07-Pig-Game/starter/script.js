76647c0f4f18c6457cb557eaf9dba2beae2b87b9
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-06-01 17:34:57 +0200

    #Project #3: Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Selecting elements
+// const player0El = document.querySelector('.player--0');
+// const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+// const current0El = document.getElementById('current--0');
+// const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+
+//Starting conditions
+// const btnNew = document.querySelector('.btn--new');
+// const btnRoll = document.querySelector('.btn--roll');
+// const btnRHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// console.log(score0El);
+// console.log(score1El);
+// console.log(diceEl);
+// score0El;
