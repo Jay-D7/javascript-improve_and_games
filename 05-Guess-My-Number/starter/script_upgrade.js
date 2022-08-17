00cd7da2bb75e6b59a91f7e9bf550c26a00b7b2e
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-17 18:25:02 +0100

    #selecting and manipulating elements & btn fnc

--- 05-Guess-My-Number/starter/script_upgrade.js
+++ 05-Guess-My-Number/starter/script_upgrade.js
@@ -1,10 +1,7 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// Select button
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess);
+});
