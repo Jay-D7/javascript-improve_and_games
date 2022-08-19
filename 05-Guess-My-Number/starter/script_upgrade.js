b02d711734b420dfa4d3031c90539ef508e94289
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-19 20:58:45 +0100

    # Handling Click Events

--- 05-Guess-My-Number/starter/script_upgrade.js
+++ 05-Guess-My-Number/starter/script_upgrade.js
@@ -2,6 +2,11 @@
 
 // Select button
 document.querySelector('.check').addEventListener('click', () => {
-  const guess = document.querySelector('.guess').value;
-  console.log(guess);
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number⛔️';
+  }
 });
