551b98be9b9e980d3b2a564be482f21ef88be744
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-22 00:27:28 +0100

    #Define variables and use Math.trunc

--- 05-Guess-My-Number/starter/script_upgrade.js
+++ 05-Guess-My-Number/starter/script_upgrade.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Define main variables
+const number = Math.trunc(Math.random() * 20); // Always between 1-20
+let score = 20;
+let highscore = 0;
+
 // Select button
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
