b8a1adaa090042f3b522a90d776409db9bdfdcd0
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-26 22:19:52 +0100

    #Scoping in practice part.1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,7 +1,27 @@
 'use strict';
 
-const logger = () => {
-  console.log(`Jakub has ${4 + 3} years old`);
-};
+///////////////////////////////////////
+// Scoping in Practice
 
-logger();
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} You are ${age}, born in ${birthYear}`;
+
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jacob';
+calcAge(1994);
+// console.log(age);
+// printAge();
