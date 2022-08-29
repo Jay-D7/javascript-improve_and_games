7a94cdfb730186297ab44d36ac7450e6ab39800d
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-29 01:19:04 +0100

    #Scoping in practice part.2(2/2) / and addword wrap on workspace vs code settings file.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,14 +7,28 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${firstName} You are ${age}, born in ${birthYear}`;
-
+    let output = `${firstName} You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassinging outer scope's variable
+      output = 'NEW OUTPUT';
+
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
     }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3)); // Without the 'use strict' mode it's working but we should always use scrit mode. so this is to keep here as reference.
+    console.log(output);
   }
   printAge();
 
