6fa12f141faac0e6442681dab44cc33e16241de6
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-08-31 19:45:05 +0100

    #Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/project/script.js
+++ 08-Behind-the-Scenes/project/script.js
@@ -3,6 +3,7 @@
 ///////////////////////////////////////
 // Scoping in Practice
 
+/* 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -39,3 +40,51 @@ const firstName = 'Jacob';
 calcAge(1994);
 // console.log(age);
 // printAge();
+
+*/
+
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jakub';
+let job = 'teacher';
+const year = 1994;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+const numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
