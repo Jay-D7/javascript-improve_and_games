6259c1533e88183581ac2b682ab0ba97461b3aa3
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-09-28 22:15:58 +0200

    #Logical Assignment Operators

--- 09-Data-Structures-Operators/project/script.js
+++ 09-Data-Structures-Operators/project/script.js
@@ -54,6 +54,39 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Logical Assignment Operators
+const rest1 = {
+  name: 'Capri',
+  // numGuest: 20,
+  numGuest: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuest = rest1.numGuest || 10;
+// rest2.numGuest = rest2.numGuest || 10;
+// rest1.numGuest ||= 10;
+// rest2.numGuest ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+///////////////////////////////////////
 // The Nullish Coalescing Operator
 
 restaurant.numGuests = 0;
@@ -63,6 +96,7 @@ console.log(guests);
 // Nullish: null and undefined (NOT 0 or '') Introduced in ES2020
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+*/
 
 /*
 ///////////////////////////////////////
