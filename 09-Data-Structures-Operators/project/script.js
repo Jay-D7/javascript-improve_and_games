b3bcdd08baa5bcb56c6c67db329edae73c71dbf8
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-09-29 17:52:44 +0200

    #Enhanced Object Literals

--- 09-Data-Structures-Operators/project/script.js
+++ 09-Data-Structures-Operators/project/script.js
@@ -4,55 +4,58 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const hours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  // day-${2+4}
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
-  order: function (starterIndex, mainIndex) {
+  // hours: hours, Old way before ES6
+  // ES6 enhanced object literals
+  hours,
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+/*
 ///////////////////////////////////////
 // The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
@@ -60,6 +63,9 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 // Forof Loop
 for (const item of menu) console.log(item);
 
+// for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`); // Short syntax 'method entries()'
+
+// Long syntax 'method entries()'
 for (const [i, el] of menu.entries()) {
   // console.log(item);
   // console.log(`${item[0] + 1}: ${item[1]}`);
@@ -72,6 +78,7 @@ console.log([...menu.entries()]);
 // for (let i = 0; i < menu.length; i++) {
 //   console.log(menu[i]);
 // }
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #1
