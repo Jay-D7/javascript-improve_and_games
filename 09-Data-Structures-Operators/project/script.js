9f13c342e44b4c4fde4281c0666f085100f0598e
Jakub.Dragowski <jakub.dragowski@wondermiles.com>
2022-10-10 00:29:00 +0200

    #Maps: Fundamentals

--- 09-Data-Structures-Operators/project/script.js
+++ 09-Data-Structures-Operators/project/script.js
@@ -56,8 +56,39 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
-// Sets
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open 🔥')
+  .set(false, 'We are close 😓');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest.get(arr));
+console.log(rest.size);
+
+/*
+///////////////////////////////////////
+// Sets
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -91,6 +122,7 @@ console.log(staffUnique);
 console.log(new Set(staff).size);
 
 console.log(new Set('jakubdragowski').size);
+*/
 
 /*
 ///////////////////////////////////////
