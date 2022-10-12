705f250993b865455edd73df9b85ab5aec5d0d40
Jakub.Dragowski <kuba.dragowski@gmail.com>
2022-10-13 01:21:11 +0200

    #Working With Strings - Part 2

--- 09-Data-Structures-Operators/project/script.js
+++ 09-Data-Structures-Operators/project/script.js
@@ -56,6 +56,88 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Working With Strings - Part 2
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalizations in name
+const passenger = 'jAkUB';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+console.log(passengerCorrect);
+
+// Function which takes any passenger name and then returns the correct one with the fixed capitalization.
+const passengerName = item => {
+  const passengerLower =
+    item[0].toLowerCase().toUpperCase() + item.toLowerCase().slice(1);
+
+  console.log(passengerLower);
+};
+passengerName('jaKuB');
+passengerName('mEFIStoteLeS');
+
+// Comparing emails
+const email = 'hello@jakub.io';
+const loginEmail = '  Hello@Jakub.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Function trim and return is false or not
+const checkEmails = item => {
+  let email = 'hello@jakub.io';
+  const normalizedEmail = item.toLowerCase().trim();
+
+  return console.log(email === normalizedEmail);
+};
+checkEmails('  Hello@Jakub.Io \n');
+checkEmails('  Hello@Jakub.Isso \n');
+
+// replacing
+const priceGB = '288,97€';
+const priceUS = priceGB.replace('€', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate')); // New method in JS
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW ARirbus family');
+}
+
+// Practice exercise
+const checkBaggage = items => {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun'))
+    console.log('You are not allowed on board');
+  else console.log('Welcome aboard!');
+};
+
+checkBaggage('I have laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+/*
+///////////////////////////////////////
 // Working With Strings - Part 1
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
@@ -96,6 +178,7 @@ console.log(new String('Jakub'));
 console.log(typeof new String('Jakub'));
 
 console.log(typeof new String('Jakub').slice(1));
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #3
