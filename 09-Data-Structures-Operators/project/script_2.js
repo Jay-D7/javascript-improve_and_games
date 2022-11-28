bf7394efaf46ccd3621c89bc10f490e45fa731cb
Jakub.Dragowski <kuba.dragowski@gmail.com>
2022-11-29 00:36:08 +0100

    #The Spread Operator (...)

--- 09-Data-Structures-Operators/project/script_2.js
+++ 09-Data-Structures-Operators/project/script_2.js
@@ -35,8 +35,76 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
 };
 
+// const size = Object.keys(restaurant).length;
+// console.log(size);
+
+///////////////////////////////////////
+// The Spread Operator (...)
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+// Logged individual elements of the array
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+// Add element
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// Join 2 arrays & more
+const menu = [...restaurant.starterMenu, ...newMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jakub';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Dragowski`); CAN'T use with template literal
+
+// Real-worlds example
+const ingrednients = [
+  // prompt(`Let's make pasta! Ingrednients 1?`),
+  // prompt(`Let's make pasta! Ingrednients 2?`),
+  // prompt(`Let's make pasta! Ingrednients 3?`),
+];
+console.log(ingrednients);
+
+// restaurant.orderPasta(ingrednients[0], ingrednients[1], ingrednients[2]); // Old way
+restaurant.orderPasta(...ingrednients); // New way ES6
+
+// Objects
+const newRestaurant = {
+  foundedIn: 1998,
+  ...restaurant,
+  founder: 'Guiseppe',
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
+///////////////////////////////////////
+// Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
@@ -49,11 +117,6 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
-const size = Object.keys(restaurant).length;
-console.log(size);
-
-///////////////////////////////////////
-// Destructuring Objects
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -81,6 +144,7 @@ const {
   fri: { open: o, close: c },
 } = hours;
 console.log(o, c);
+*/
 
 /*
 ///////////////////////////////////////
