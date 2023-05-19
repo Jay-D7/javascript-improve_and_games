f16f94132b8c0a0ed7905293c3106aa8faee9952
Jakub.Dragowski <kuba.dragowski@gmail.com>
2023-05-20 01:41:43 +0200

    #Destructuring Arrays

--- 09-Data-Structures-Operators/project/script.js
+++ 09-Data-Structures-Operators/project/script.js
@@ -999,6 +999,8 @@ const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
+console.log(a, b, c);
+
 // Destructuring ES6
 const [x, y, z] = arr;
 console.log(x, y, z);
--- 09-Data-Structures-Operators/project/script_2.js
+++ 09-Data-Structures-Operators/project/script_2.js
@@ -172,7 +172,6 @@ const {
 console.log(o, c);
 */
 
-/*
 ///////////////////////////////////////
 // Destructuring Arrays
 const arr = [2, 3, 4];
@@ -180,6 +179,9 @@ const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
+console.log(a, b, c);
+
+// Destructuring ES6
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
@@ -210,29 +212,3 @@ console.log(i, j, k);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
-*/
-
-// const arr = [3, 4, 5];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
-
-// console.log(a, b, c);
-
-// const [x, y, z] = arr;
-
-// console.log(x, y, z);
-// console.log(arr);
-
-const arr = [3, 4, 5];
-
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-
-console.log(a, b, c);
-
-const [x, y, z] = arr;
-
-console.log(x, y, z);
-console.log(arr);
