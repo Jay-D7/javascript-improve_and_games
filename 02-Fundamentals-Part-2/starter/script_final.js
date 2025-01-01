174711b38a6e93202cbf06cec12d5ea24b439221
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-01-01 01:26:16 +0100

    #Coding Challenge 1

--- 02-Fundamentals-Part-2/starter/script_final.js
+++ 02-Fundamentals-Part-2/starter/script_final.js
@@ -81,13 +81,7 @@ console.log(
 
 // LECTURE: Functions Calling Other Functions
 console.log(`\nLecture 4: Functions Calling Other Functions`);
-// 1. Create a function called 'describePopulation'. Use the function type you
-// like the most. This function takes in two arguments: 'country' and
-// 'population', and returns a string like this: 'China has 1441 million people,
-// which is about 18.2% of the world.'
-// 2. To calculate the percentage, 'describePopulation' call the
-// 'percentageOfWorld1' you created earlier
-// 3. Call 'describePopulation' with data for 3 countries of your choice
+
 const describePopulation = (country, population) => {
   let percentage = percentageOfWorld1(population).toFixed(2);
 
@@ -99,3 +93,49 @@ console.log(
   describePopulation('Germany', 83) + '\n',
   describePopulation('Ukraine', 44)
 );
+
+// Coding Challenge #1
+console.log('\nCoding Challenge #1');
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+console.log(calcAverage(3, 4, 5));
+
+// 2. Use the function to calculate the average for both teams
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+
+// 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins wins ${avgDolphins} vs ${avgKoalas}`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas wins ${avgKoalas} vs ${avgDolphins}`);
+  } else {
+    console.log(`No team wins!`);
+  }
+};
+
+// 4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+checkWinner(avgDolphins1, avgKoalas1);
+checkWinner(avgDolphins2, avgKoalas2);
